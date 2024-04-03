b141ee88d5b7ce2dd2f0e3d5293e7a5f0743dc67
raulemm7 <86725051+raulemm7@users.noreply.github.com>
2024-04-03 22:57:16 +0300

    Update service.c

--- Semestrul 2/OOP/lab 03/service.c
+++ Semestrul 2/OOP/lab 03/service.c
@@ -1,15 +1,16 @@
+@ -0,0 +1,120 @@
 //
 // Created by raulo on 3/21/2024.
 //
 
 #include "service.h"
 
-Repo* adauga_o_noua_cheltuiala(Repo* storage, int id_cheltuiala, int numar_apartament, int suma_cheltuiala,
+Repo adauga_o_noua_cheltuiala(Repo storage, int id_cheltuiala, int numar_apartament, int suma_cheltuiala,
                               char* tip_cheltuiala) {
     // verific daca aceasta cheltuiala exista deja
     if(validare_cheltuiala_existenta(storage, numar_apartament, suma_cheltuiala, tip_cheltuiala)) {
         // creez o noua cheltuiala
-        cheltuiala* chelt = create_cheltuiala(id_cheltuiala, numar_apartament, suma_cheltuiala, tip_cheltuiala);
+        cheltuiala chelt = create_cheltuiala(id_cheltuiala, numar_apartament, suma_cheltuiala, tip_cheltuiala);
 
         // adaug cheltuiala in storage
         // verific daca mai am loc, daca nu realoc memorie pentru storage
@@ -31,13 +32,13 @@ Repo* adauga_o_noua_cheltuiala(Repo* storage, int id_cheltuiala, int numar_apart
     return storage;
 }
 
-Repo* modifica_cheltuiala_existenta(Repo* storage, int id_ales, int suma_noua,
+Repo modifica_cheltuiala_existenta(Repo storage, int id_ales, int suma_noua,
                                    char *tip_cheltuiala_nou) {
     // verific daca pentru datele introduse exista deja inregistrari
     int numar_ap = get_numar_apartament(get_cheltuiala_by_id(storage, id_ales));
     if(validare_cheltuiala_existenta(storage, numar_ap, suma_noua, tip_cheltuiala_nou)) {
         // actualizez cheltuiala
-        cheltuiala* chelt = get_cheltuiala_by_id(storage, id_ales);
+        cheltuiala chelt = get_cheltuiala_by_id(storage, id_ales);
         chelt = modifica_informatii_cheltuiala(chelt, suma_noua, tip_cheltuiala_nou);
 
         // actualizez storage-ul
@@ -54,7 +55,7 @@ Repo* modifica_cheltuiala_existenta(Repo* storage, int id_ales, int suma_noua,
     return storage;
 }
 
-Repo* sterge_cheltuiala_din_storage(Repo* storage, int id_ales) {
+Repo sterge_cheltuiala_din_storage(Repo storage, int id_ales) {
     // sterg efectiv
     storage = delete_cheltuiala(storage, id_ales);
 
@@ -69,7 +70,7 @@ void run_service_tests() {
 }
 
 void test_adaugare() {
-    Repo* storage = creeaza_repository();
+    Repo storage = creeaza_repository();
 
     char *tip = (char*)malloc(sizeof(char) * 32);
     strcpy(tip, "internet");
@@ -85,7 +86,7 @@ void test_adaugare() {
 }
 
 void test_modificare() {
-    Repo* storage = creeaza_repository();
+    Repo storage = creeaza_repository();
 
     char *tip = (char*)malloc(sizeof(char) * 32);
     strcpy(tip, "internet");
@@ -95,7 +96,7 @@ void test_modificare() {
     strcpy(new_tip, "apa");
     storage = modifica_cheltuiala_existenta(storage, 0, 460, new_tip);
 
-    cheltuiala* chelt = get_cheltuiala_by_id(storage, 0);
+    cheltuiala chelt = get_cheltuiala_by_id(storage, 0);
 
     assert(get_suma_cheltuiala(chelt) == 460);
     char *tip_from_chelt = get_tip_cheltuiala(chelt);
@@ -106,7 +107,7 @@ void test_modificare() {
 }
 
 void test_stergere() {
-    Repo* storage = creeaza_repository();
+    Repo storage = creeaza_repository();
 
     char *tip = (char*)malloc(sizeof(char) * 32);
     strcpy(tip, "internet");
