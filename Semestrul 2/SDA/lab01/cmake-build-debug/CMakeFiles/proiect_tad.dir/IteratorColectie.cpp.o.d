729c4b8c1f8c952f60a82ad909beddaa98971160
raulemm7 <86725051+raulemm7@users.noreply.github.com>
2024-04-11 00:00:24 +0300

    Delete Semestrul 2/SDA/lab01/cmake-build-debug directory

--- Semestrul 2/SDA/lab01/cmake-build-debug/CMakeFiles/proiect_tad.dir/IteratorColectie.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/proiect_tad.dir/IteratorColectie.cpp.o: \
- /mnt/d/proiect_tad/IteratorColectie.cpp /usr/include/stdc-predef.h \
- /mnt/d/proiect_tad/IteratorColectie.h /mnt/d/proiect_tad/Colectie.h
--- Semestrul 2/SDA/lab01/cmake-build-debug/CMakeFiles/proiect_tad.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for proiect_tad.
-# This may be replaced when dependencies are built.
