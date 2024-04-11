c971715f41a508e4d7b11c26cf89d2e4e6ed0456
raulemm7 <86725051+raulemm7@users.noreply.github.com>
2024-04-11 14:31:29 +0300

    Create main.cpp

---
+++ Semestrul 2/Algoritmica grafurilor/practic/problema practic 3/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int nrCopii, nrRelatii;
+int mat[1005][1005];
+
+int vizitat[1005];
+
+void dfs(int nod_curent, int grup_nod) {
+    vizitat[nod_curent] = grup_nod;
+    for(int i = 1; i <= nrCopii; i++) {
+        if(mat[nod_curent][i] and !vizitat[i])
+            dfs(i, grup_nod);
+    }
+}
+
+/*
+void reconstituire_traseu(int nod_curent) {
+    if(nod_curent != 0) {
+        reconstituire_traseu(parinte[nod_curent]);
+        cout << nod_curent << " ";
+    }
+}
+*/
+
+int main() {
+    int x, y;
+
+    ifstream fin("input.txt");
+    fin >> nrCopii >> nrRelatii;
+    for(int i = 1; i <= nrRelatii; i++) {
+        fin >> x >> y;
+        mat[x][y] = mat[y][x] = 1;
+    }
+
+    int nr_grupuri = 0;
+    for(int i = 1; i <= nrCopii; i++) {
+        if(!vizitat[i]) {
+            nr_grupuri++;
+            dfs(i, nr_grupuri);
+        }
+    }
+    cout << "Exista " << nr_grupuri << " de prieteni\n";
+    for(int i = 1; i <= nr_grupuri; i++) {
+        cout << "Grupul " << i << ": ";
+        for(int j = 1; j <= nrCopii; j++) {
+            if(vizitat[j] == i) {
+                cout << j << " ";
+            }
+        }
+        cout << '\n';
+    }
+
+    fin.close();
+    return 0;
+}
