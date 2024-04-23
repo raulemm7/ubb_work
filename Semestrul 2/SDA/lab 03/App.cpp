1bb0924ee681b485468904c0bbdfcc1532416264
raulemm7 <86725051+raulemm7@users.noreply.github.com>
2024-04-24 01:23:27 +0300

    Create App.cpp

---
+++ Semestrul 2/SDA/lab 03/App.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include "TestExtins.h"
+#include "TestScurt.h"
+
+#include "Colectie.h"
+
+using namespace std;
+
+
+int main() {
+	testAll();
+	testAllExtins();
+    cout<<"Toate testele au fost finalizate cu succes!";
+
+    /*Colectie c;
+    c.adauga(1);
+    c.afiseaza_elemente();
+    cout << "\n";
+    c.adauga(2);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(3);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(4);
+    c.afiseaza_elemente();
+    cout << "\n";
+    c.adauga(5);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(6);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(7);
+    c.afiseaza_elemente();
+    cout << "\n";
+    c.adauga(8);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(9);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(10);
+    c.afiseaza_elemente();
+    cout << "\n";
+    c.adauga(11);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(12);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(13);
+    c.afiseaza_elemente();
+    cout << "\n";
+    c.adauga(14);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(15);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(16);
+    c.afiseaza_elemente();
+    cout << "\n";
+    c.adauga(17);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(18);
+    c.afiseaza_elemente();
+    cout << '\n';
+    cout << c.dim();
+
+    c.adauga(1);
+    c.afiseaza_elemente();
+    cout << "\n";
+    c.adauga(2);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(3);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(1);
+    c.afiseaza_elemente();
+    cout << "\n";
+    c.adauga(2);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(3);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(1);
+    c.afiseaza_elemente();
+    cout << "\n";
+    c.adauga(2);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(3);
+    c.afiseaza_elemente();
+    cout << '\n';
+
+    cout << c.dim();
+
+    c.adauga(-1);
+    c.afiseaza_elemente();
+    cout << "\n";
+    c.adauga(-2);
+    c.afiseaza_elemente();
+    cout << '\n';
+    c.adauga(-3);
+    c.afiseaza_elemente();
+    cout << '\n';
+
+    cout << c.dim() << '\n';
+
+    c.sterge(3);
+    cout << c.dim() << '\n';
+    c.sterge(3);
+    cout << c.dim() << '\n';
+    c.sterge(3);
+    cout << c.dim() << '\n';
+    c.sterge(3);
+    cout << c.dim() << '\n';
+    c.sterge(3);
+    cout << c.dim() << '\n';
+    c.sterge(3);
+    cout << c.dim() << '\n';
+
+    c.afiseaza_elemente();*/
+}
