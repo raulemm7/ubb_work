aecf5a5888ce0cb8a96cdbc96df15475b03677c2
raulemm7 <86725051+raulemm7@users.noreply.github.com>
2024-05-13 00:57:13 +0300

    Create main.cpp

---
+++ Semestrul 2/OOP/lab09/main.cpp
@@ -0,0 +1,13 @@
+#include "tests.h"
+
+int main() {
+    test_all();
+    auto* repo=new FileRepo{"data.txt"};
+    Validator valid;
+    Service srv{repo,valid};
+
+    auto master=Console(srv);
+    master.show_ui();
+    delete repo;
+    return 0;
+}
