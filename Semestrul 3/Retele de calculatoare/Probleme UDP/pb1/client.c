b925d5d086a001e2672d906a97925b747c07c632
raulemm7 <86725051+raulemm7@users.noreply.github.com>
2024-11-11 22:54:45 +0200

    Create client.c

---
+++ Semestrul 3/Retele de calculatoare/Probleme UDP/pb1/client.c
@@ -0,0 +1,47 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+
+void trimite_server(int c, struct sockaddr_in server) {
+        uint16_t a, b, suma;
+        printf("Numarul a: ");
+        scanf("%hu", &a);
+        a = htons(a);
+
+        printf("Numarul b: ");
+        scanf("%hu", &b);
+        b = htons(b);
+
+        sendto(c, &a, sizeof(a), 0, (struct sockaddr*) &server, sizeof(server));
+        sendto(c, &b, sizeof(b), 0, (struct sockaddr*) &server, sizeof(server));
+
+        int l = sizeof(server);
+        recvfrom(c, &suma, sizeof(suma), MSG_WAITALL, (struct sockaddr*) &server, &l);
+        suma = ntohs(suma);
+        printf("Suma este: %hu\n", suma);
+}
+
+int main(int argc, char* argv[]) {
+        int c;
+        struct sockaddr_in server;
+        uint16_t a, b, suma, i, k;
+
+
+        c = socket(AF_INET, SOCK_DGRAM, 0);
+        if (c < 0) {
+                printf("Eroare la crearea socketului client\n");
+                return 1;
+        }
+
+        memset(&server, 0, sizeof(server));
+        server.sin_port = htons(atoi(argv[1]));
+        server.sin_family = AF_INET;
+        server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+        trimite_server(c, server);
+
+        close(c);
+}
